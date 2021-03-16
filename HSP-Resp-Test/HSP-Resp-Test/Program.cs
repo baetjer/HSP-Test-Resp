691d5f3240b0051974a9f36f08f36932af5a142c
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-03-16 20:41:19 +0100

    Update Program.cs

--- HSP-Resp-Test/HSP-Resp-Test/Program.cs
+++ HSP-Resp-Test/HSP-Resp-Test/Program.cs
@@ -11,14 +11,7 @@ namespace HSP_Resp_Test
         static void Main(string[] args)
         {
 
-            int i = 1;
-            Console.WriteLine("       Willkommen Zur Gruppe F");
-            Console.WriteLine("**********************************");
-            Console.WriteLine("       Hallo Tim");
-            Console.WriteLine("       Hallo Olä");
-            Console.WriteLine("       你好 Qian");
-            Console.WriteLine("press F to pay respect");
-            Console.WriteLine("ye");
+            
 
             Console.ReadKey();
             
