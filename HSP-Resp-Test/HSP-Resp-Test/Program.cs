1721e0f9b55051a9567f8004b3c3ccbd531ece87
Mars0922 <80465263+Mars0922@users.noreply.github.com>
2021-03-16 20:44:20 +0100

    Update Program.cs

--- HSP-Resp-Test/HSP-Resp-Test/Program.cs
+++ HSP-Resp-Test/HSP-Resp-Test/Program.cs
@@ -11,7 +11,18 @@ namespace HSP_Resp_Test
         static void Main(string[] args)
         {
 
+<<<<<<< Updated upstream
             
+=======
+            int i = 1;
+            Console.WriteLine("       Willkommen Zur Gruppe F");
+            Console.WriteLine("**********************************");
+            Console.WriteLine("       Hallo Tim");
+            Console.WriteLine("       Hallo Ole");
+            Console.WriteLine("       你好 Qian");
+            Console.WriteLine("press F to pay respect");
+            Console.WriteLine("ye");
+>>>>>>> Stashed changes
 
             Console.ReadKey();
             
