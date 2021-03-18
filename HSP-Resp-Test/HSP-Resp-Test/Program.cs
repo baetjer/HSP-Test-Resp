5baf767a39c62815542c0dd2b0d8f1c9a8886572
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-03-18 09:04:05 +0100

    Update Program.cs

--- HSP-Resp-Test/HSP-Resp-Test/Program.cs
+++ HSP-Resp-Test/HSP-Resp-Test/Program.cs
@@ -19,7 +19,7 @@ namespace HSP_Resp_Test
             Console.WriteLine("       Hallo Ole");
             Console.WriteLine("       你好 Qian");
             Console.WriteLine("       Hallo Fabian");
-            Console.WriteLine("press F to pay respect");
+            Console.WriteLine("press F to pay respects");
             Console.WriteLine("press F to bazehlen respect");
             Console.WriteLine("whatsupp");
             Console.WriteLine("oof");
