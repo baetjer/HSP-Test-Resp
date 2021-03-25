03090ae9db5d0bd2b047bfc927f915a8384795dd
Mars0922 <80465263+Mars0922@users.noreply.github.com>
2021-03-25 10:59:01 +0100

    Update Program.cs

--- HSP-Resp-Test/HSP-Resp-Test/Program.cs
+++ HSP-Resp-Test/HSP-Resp-Test/Program.cs
@@ -22,11 +22,8 @@ namespace HSP_Resp_Test
             Console.WriteLine("press F to pay respects");
             Console.WriteLine("press F to bazehlen respect");
             Console.WriteLine("Good morning");
-<<<<<<< Updated upstream
-            Console.WriteLine("moin jungens");
-=======
-            Console.WriteLine("moin ");
->>>>>>> Stashed changes
+            Console.WriteLine("moin jungs");
+            Console.WriteLine("moin");
             Console.WriteLine("هههههههههه");
 
             Console.ReadKey();
