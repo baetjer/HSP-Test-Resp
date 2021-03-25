fc3f09c2939c5004ac57d880450b116e5c77d798
Fabian-Hienen <80354764+Fabian-Hienen@users.noreply.github.com>
2021-03-25 11:01:42 +0100

    Update Program.cs

--- HSP-Resp-Test/HSP-Resp-Test/Program.cs
+++ HSP-Resp-Test/HSP-Resp-Test/Program.cs
@@ -22,7 +22,7 @@ namespace HSP_Resp_Test
             Console.WriteLine("press F to pay respects");
             Console.WriteLine("press F to bazehlen respect");
             Console.WriteLine("Good morning");
-            Console.WriteLine("moin jungs");
+            Console.WriteLine("guten morgen jungs");
             Console.WriteLine("moin");
             Console.WriteLine("هههههههههه");
 
