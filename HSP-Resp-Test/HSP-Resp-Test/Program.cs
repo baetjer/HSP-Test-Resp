a773e9e01109820b240f06d4533f24adfdc8c3f7
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-03-25 11:03:44 +0100

    Konflikt erzeugt

--- HSP-Resp-Test/HSP-Resp-Test/Program.cs
+++ HSP-Resp-Test/HSP-Resp-Test/Program.cs
@@ -22,8 +22,12 @@ namespace HSP_Resp_Test
             Console.WriteLine("press F to pay respects");
             Console.WriteLine("press F to bazehlen respect");
             Console.WriteLine("Good morning");
+<<<<<<< Updated upstream
             Console.WriteLine("guten morgen jungs");
             Console.WriteLine("moin");
+=======
+            Console.WriteLine("moin jungens");
+>>>>>>> Stashed changes
             Console.WriteLine("هههههههههه");
 
             Console.ReadKey();
