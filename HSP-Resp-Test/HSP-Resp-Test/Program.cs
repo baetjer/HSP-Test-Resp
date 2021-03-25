adbbb04ed76b4e7f5b83f92f13db4360cb4f1a31
timwurbs <tim.wurbs@student.jade-hs.de>
2021-03-25 10:45:55 +0100

    Update Program.cs

--- HSP-Resp-Test/HSP-Resp-Test/Program.cs
+++ HSP-Resp-Test/HSP-Resp-Test/Program.cs
@@ -22,7 +22,7 @@ namespace HSP_Resp_Test
             Console.WriteLine("press F to pay respects");
             Console.WriteLine("press F to bazehlen respect");
             Console.WriteLine("Good morning");
-            Console.WriteLine("moin");
+            Console.WriteLine("moin jungs ");
             Console.WriteLine("هههههههههه");
 
             Console.ReadKey();
