433ff5499d2ed394b9185500d95eb218d3b68851
Mars0922 <80465263+Mars0922@users.noreply.github.com>
2021-03-25 11:41:43 +0100

    konflik

--- HSP-Resp-Test/HSP-Resp-Test/Program.cs
+++ HSP-Resp-Test/HSP-Resp-Test/Program.cs
@@ -21,8 +21,13 @@ namespace HSP_Resp_Test
             Console.WriteLine("       Hallo Fabian");
             Console.WriteLine("press F to pay respects");
             Console.WriteLine("press F to bazehlen respect");
+<<<<<<< Updated upstream
             Console.WriteLine("guten morgen");
             Console.WriteLine("moin");
+=======
+            Console.WriteLine("Good morning");
+            Console.WriteLine("moin jungens");
+>>>>>>> Stashed changes
             Console.WriteLine("هههههههههه");
 
             Console.ReadKey();
