5c316ec17734aab01749e0514adc9e40a695d50d
timwurbs <tim.wurbs@student.jade-hs.de>
2021-03-25 11:44:21 +0100

    Update Program.cs

--- HSP-Resp-Test/HSP-Resp-Test/Program.cs
+++ HSP-Resp-Test/HSP-Resp-Test/Program.cs
@@ -21,13 +21,9 @@ namespace HSP_Resp_Test
             Console.WriteLine("       Hallo Fabian");
             Console.WriteLine("press F to pay respects");
             Console.WriteLine("press F to bazehlen respect");
-<<<<<<< Updated upstream
+
             Console.WriteLine("guten morgen");
             Console.WriteLine("moin");
-=======
-            Console.WriteLine("Good morning");
-            Console.WriteLine("moin jungens");
->>>>>>> Stashed changes
             Console.WriteLine("هههههههههه");
 
             Console.ReadKey();
